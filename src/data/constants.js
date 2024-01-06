94429f13e54051e5bb64a183b57efda398c16c78
Harnil1602 <harnilshah1602@gmail.com>
2024-01-07 04:02:16 +0530

    Changed the constants file

--- src/data/constants.js
+++ src/data/constants.js
@@ -6,10 +6,10 @@ export const Bio = {
     "Python Developer",
   ],
   description:
-    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    "I am a motivated and versatile individual who believes in fueling innovation through passion, dedication, and continuous learning. Committed to delivering excellence and embracing challenges with a positive mindset.",
   github: "https://github.com/Harnil1602",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/12mvtTFKeIML4rPrbBlPKGWEUEd4T-iGu/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/harnil-shah-6a203b209/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/rishav_chanda/",
@@ -218,8 +218,8 @@ export const education = [
     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
     school: "Charotar University of Science & Technology",
     date: "Nov 2020 - may 2024",
-    grade: "8.82 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    grade: "8.90 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at CHARUSAT, Anand. I have completed 7 semesters and have a CGPA of 8.90. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
     degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
   },
   {
@@ -227,18 +227,18 @@ export const education = [
     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
     school: "A-one High School",
     date: "Apr 2019 - Apr 2025",
-    grade: "88.2%",
-    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    grade: "70%",
+    desc: "I completed my class 12th high school education at AONE HIGH SCHOOL, Ahmedabad, where I studied in Science stream.",
     degree: "ISC(XII), Science with Computer",
   },
   {
     id: 2,
     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
+    school: "Nirman High School",
     date: "Apr 2017 - Apr 2019",
-    grade: "92.3%",
-    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
-    degree: "ICSC(X), Science with Computer",
+    grade: "85%",
+    desc: "I completed my class 10th education at Nirman High School, Ahmedabad, where my keen interest was in Mathematics.",
+    degree: "Nurs. - X",
   },
 ];
 
